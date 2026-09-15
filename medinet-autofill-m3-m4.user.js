dc594e5fd901c9a603ec65deafa531be3a03e105
VietsDragon <tranxuancuonglxag@gmail.com>
2026-09-15 15:01:46 +0700

    Bump version from 7.41 to 7.42

--- medinet-autofill-m3-m4.user.js
+++ medinet-autofill-m3-m4.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto KSK TD
 // @namespace    medinet-autofill-m3-m4
-// @version      7.41
+// @version      7.42
 // @description  Tự Động Điền KSK TD
 // @match        https://quanlyskcd.medinet.org.vn/*
 // @grant        none
